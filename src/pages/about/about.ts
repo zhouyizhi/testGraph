e52851d8a07ce3e3ade75e14d71eda4e4c88d2fb
zhouyizhi <finalcapmaker@gmail.com>
2016-11-15 18:38:18 +0900

    線改修 2016/11/15 up

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -29,7 +29,7 @@ export class AboutPage {
                 labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
                 datasets: [{
                     label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
+                    data: [12, 19, 3, 5, 2, 100],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -37,6 +37,27 @@ export class AboutPage {
                         'rgba(75, 192, 192, 0.2)',
                         'rgba(153, 102, 255, 0.2)',
                         'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                },
+                {
+                    label: '# of Notes',
+                    data: [12, 19, 3, 50, 20, 200],
+                    backgroundColor: [
+                        'rgba(0, 0, 0, 0.2)',
+                        'rgba(0, 0, 0, 0.2)',
+                        'rgba(0, 0, 0, 0.2)',
+                        'rgba(0, 0, 0, 0.2)',
+                        'rgba(0, 0, 0, 0.2)',
+                        'rgba(0, 0, 0, 0.2)'
                     ],
                     borderColor: [
                         'rgba(255,99,132,1)',
