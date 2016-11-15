e375dc0b1a6a1e90fe20bd13cbd316ec63476006
zhouyizhi <finalcapmaker@gmail.com>
2016-11-15 18:57:30 +0900

    update 2016/11/15 up

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import * as ChartJs from 'chart.js'; // 导入chart.js
-import { $ } from 'jQuery';
 
 
 @Component({
@@ -19,10 +18,12 @@ export class AboutPage {
   }
 
   ngOnInit() {
-    alert("1");
+      this.drawLine();
+      this.drawBar();
+  }
+
+  drawLine() {
     var canvas = <HTMLCanvasElement> document.getElementById("myChart");
-    alert(canvas);
-    this.ctx = canvas.getContext("2d");
     this.ctx = canvas.getContext("2d");
     ChartJs.Line(this.ctx,{
             data: {
@@ -80,36 +81,25 @@ export class AboutPage {
                 }
             }
         })
-        alert("1");
-  }
+    }
 
-  testsure() {
-    var canvas = <HTMLCanvasElement> document.getElementById("myChart");
-     alert(canvas);
+    drawBar() {
+        var canvas = <HTMLCanvasElement> document.getElementById("myChart2");
     this.ctx = canvas.getContext("2d");
-    ChartJs.Line(this.ctx,{
+    ChartJs.Bar(this.ctx,{
             data: {
                 labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
                 datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
+                  label: '# of One',
+                  fillColor : 'rgba(220,220,220,0.5)',
+                  strokeColor : 'rgba(220,220,220,1)',
+                  data : [65,59,90,81,56,55,40]
+                },
+                {
+                  label: '# of Two',
+                  fillColor : 'rgba(151,0,0,0.5)',
+                  strokeColor : 'rgba(151,187,205,1)',
+                  data : [28,48,40,19,96,27,100]
                 }]
             },
             options: {
@@ -122,27 +112,7 @@ export class AboutPage {
                 }
             }
         })
-        alert("1");
     }
     
-    //This will get the first returned node in the jQuery collection.
-    /*this.myNewChart = new Chart(this.ctx);
-    var data = {
-      labels : ["January","February","March","April","May","June","July"],
-      datasets : [
-        {
-          fillColor : "rgba(220,220,220,0.5)",
-          strokeColor : "rgba(220,220,220,1)",
-          data : [65,59,90,81,56,55,40]
-        },
-        {
-          fillColor : "rgba(151,187,205,0.5)",
-          strokeColor : "rgba(151,187,205,1)",
-          data : [28,48,40,19,96,27,100]
-        }
-      ]
-    }
-    this.myNewChart.Bar(data);
-  } */
-
+   
 }
